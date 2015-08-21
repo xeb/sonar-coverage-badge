6d88034b4b16224e2877e27d877ba6d70ecfb3b5
Mark Kockerbeck <mark@kockerbeck.com>
2015-08-21 00:45:11 -0700

    Adding a negative test

--- test/getcoverage.test.js
+++ test/getcoverage.test.js
@@ -14,6 +14,7 @@ describe('Get Coverage', function() {
       done();
     });
   });
+
   it('should send parse error out', function(done) {
 
       var c = badger.GetCoverage('nemo.fasdfdsfasdfsd', undefined, 'org.codehaus.sonar-plugins.php:parent', 'coverage',
@@ -25,4 +26,18 @@ describe('Get Coverage', function() {
         done();
       });
     });
+
+
+    it('should handle an error', function(done) {
+
+        var c = badger.GetCoverage('127.0.0.1', undefined, 'none', 'none',
+        function(d){ // success
+          d.should.equal(95.4);
+          done();
+        }, function(e){ // error
+          e.toString().should.equal("Error: connect ECONNREFUSED");
+          done();
+        });
+      });
+
 });
